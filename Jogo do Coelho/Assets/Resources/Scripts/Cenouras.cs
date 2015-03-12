0cfbfa2023723843f94f009f250fae62b9169314
anaflv <ana-flavia@hotmail.co.uk>
2015-03-12 17:25:43 -0300

    .

--- Jogo do Coelho/Assets/Resources/Scripts/Cenouras.cs
+++ Jogo do Coelho/Assets/Resources/Scripts/Cenouras.cs
@@ -5,7 +5,6 @@ public class Cenouras : MonoBehaviour {
 
 	void Update(){
 		
-		
 		Debug.Log("cenoura");
 		transform.Rotate(Vector3.forward * Time.deltaTime * 50, Space.World);
 	
@@ -13,6 +12,9 @@ public class Cenouras : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other){
 	
+		if(other.GetComponent<AudioSource>().isPlaying == false){
+			other.GetComponent<AudioSource>().Play();
+		}
 		Destroy(gameObject);
 	}
 }
--- Jogo do Coelho/Assets/Resources/Scripts/Cogumelos.cs
+++ Jogo do Coelho/Assets/Resources/Scripts/Cogumelos.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Cogumelos : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other) {
+		
+		other.GetComponent<AudioSource>().Play();
 		Destroy(gameObject);
 	}
 }
